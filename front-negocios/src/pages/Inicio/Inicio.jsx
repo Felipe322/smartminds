07b81992149b2b36d48dd4bc56daf87522942e80
FelipeD <37180544@uaz.edu.mx>
2020-10-11 16:11:26 -0500

    Se agrego el componente de la carta, en la pestaña de inicio

--- front-negocios/src/pages/Inicio/Inicio.jsx
+++ front-negocios/src/pages/Inicio/Inicio.jsx
@@ -2,18 +2,65 @@ import { Button } from '@material-ui/core'
 import React from 'react'
 import Titulo from '../../components/Titulo'
 import './Inicio.css'
+import './Cards.css'
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { blue, pink } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 
-function Inicio() {
-    return (
-        <div>
-            <Titulo titulo="Inicio"/>
-            <div className="contenedorBoton">
-                <Button variant="contained" color="primary" className="boton">
-                Primary
-                </Button>
-            </div>
-        </div>
-    )
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      maxWidth: 300,
+    },
+    media: {
+      height: 0,
+      paddingTop: '55%',
+    },
+    avatar: {
+      backgroundColor: blue[500],//Imagen de la base
+    },
+  }),
+);
+
+export default function RecipeReviewCard() {
+  const classes = useStyles();
+  return (
+    <div className='contenedorCards'>
+        <Card className={classes.root} className='sombraCards'>
+        <CardHeader
+            avatar={
+            <Avatar aria-label="recipe" className={classes.avatar}>
+                T
+            </Avatar>
+            }
+            title="Zapatería Limones"//Nombre de la empresa de la base de datos.
+            subheader="Los mejores zapatos de la region"//Subtitulo de la base de datos. 
+        />
+        <CardMedia
+            className={classes.media}
+            image="https://e00-elmundo.uecdn.es/elmundo/imagenes/2013/07/09/economia/1373363070_0.jpg"//Imagen de la base
+        />
+        <CardContent>
+            <Typography variant="body2" color="textSecondary" component="p">
+            Zapatos para toda la familia, con todos los gusto. Nuestros clientes
+            siempre están satisfechos con el producto y con el trato.//Breve descripción de la empresa. 
+            </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+            <IconButton>
+            <FavoriteIcon color="primary"/>
+            </IconButton>
+        </CardActions>
+        </Card>
+    </div>
+  );
 }
-
-export default Inicio
